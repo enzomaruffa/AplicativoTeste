2159aeb68907589380e49ffaff17773bc7798cc8
Enzo Maruffa Moreira <enzomm1999@gmail.com>
2021-06-15 10:32:46 -0300

    Initial Commit

---
+++ AplicativoTeste/AplicativoTesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AplicativoTesteApp.swift
+//  AplicativoTeste
+//
+//  Created by Enzo Maruffa Moreira on 15/06/21.
+//
+
+import SwiftUI
+
+@main
+struct AplicativoTesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AplicativoTeste/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AplicativoTeste
+//
+//  Created by Enzo Maruffa Moreira on 15/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
